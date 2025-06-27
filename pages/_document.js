7df149d99e7c180b60dfa4b2eabcebe5601bec71
GavinBoller <gavinboller@outlook.com>
2025-06-27 12:10:19 +1000

    500 and manifest

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* This is the main icon for browsers and other platforms */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/app-icon.svg" type="image/svg+xml" />
-        
+
         {/* This is the specific icon for when you "Add to Home Screen" on an iPhone */}
         <link rel="apple-touch-icon" href="/app-icon.svg" />
+
+        {/* Web App Manifest for PWA features */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1A1A1A" />
       </Head>
       <body>
         <Main />
