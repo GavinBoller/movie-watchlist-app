b3c90118f9eab7dd4d77c465d2ffd88e49f4e01f
GavinBoller <gavinboller@outlook.com>
2025-07-08 13:17:20 +1000

    offline

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,29 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self' 'sha256-sJkK5W2Z9NEZr2kbdPJT3VH4QoHoki2l+KPwV985W74=';
+      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+      font-src 'self' https://fonts.gstatic.com;
+      img-src 'self' data: https://image.tmdb.org;
+      connect-src 'self' https://vitals.vercel-insights.com;
+      frame-src 'self';
+    `;
+
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
